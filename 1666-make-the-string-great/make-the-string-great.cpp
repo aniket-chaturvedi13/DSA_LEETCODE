a597350f35336a99b6d468cdc73ba3b9f5a0303e
Aniket Chaturvedi <aniketchaturvedi348@gmail.com>
2026-09-09 09:43:41 +0530

    Time: 4 ms (2.93%) | Memory: 8.3 MB (84.80%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeGood(string s) {
+
+        string result = " ";
+
+        
+        for(char &ch : s) {
+
+            if(result.back() + 32 == ch || result.back() - 32 == ch) result.pop_back();
+
+            else result.push_back(ch);
+        }
+        
+
+        return result.substr(1);
+    }
+};
